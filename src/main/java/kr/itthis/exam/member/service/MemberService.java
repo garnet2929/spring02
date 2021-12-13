ad08f8c1fa39442e895b46e5be597bc2b9badf89
itthisgo <itthisgo@gmail.com>
2021-12-13 21:30:14 +0900

    ready

--- src/main/java/kr/itthis/exam/member/service/MemberService.java
+++ src/main/java/kr/itthis/exam/member/service/MemberService.java
@@ -6,10 +6,10 @@ import kr.itthis.exam.member.domain.Member;
 import kr.itthis.exam.member.domain.MemberRegistCommand;
 
 public interface MemberService {
-	public boolean regist(MemberRegistCommand memberCommand);
+	public boolean regist(MemberRegistCommand memberRegistCommand);
 	public List<Member> listAll();
 	public Member search(String email, String pwd);
-	public boolean edit(MemberRegistCommand memberCommand, String oldPwd);
+	public boolean edit(MemberRegistCommand memberRegistCommand, String oldPwd);
 	public boolean remove(String email, String pwd);
 }
 
